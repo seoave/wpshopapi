72a4852e29ce83336f08d4c1d235e267182a248f
seoave <seo.ave@gmail.com>
2023-08-17 02:52:15 +0300

    first commit

---
+++ wpshop.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name:       WPshop
+ * Plugin URI:        https://wpshop.example.com
+ * Description:       Simple cart.
+ * Version:           1.0.0
+ * Requires at least: 6.2
+ * Requires PHP:      7.2
+ * Author:            seoave
+ */
+
